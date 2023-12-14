fa7c6f06bd4d703328871ca23d8db960b84c567f
weoamathior <weoamathior@gmail.com>
2023-12-14 07:06:50 -0600

    demo of ldap encoder

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Bean
     PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new LdapShaPasswordEncoder();
     }
 
     @Override
--- src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
+++ src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
@@ -1,13 +1,30 @@
 package guru.sfg.brewery.web.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.util.DigestUtils;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class PasswordEncodingTest {
 
     static final String  PASSWORD = "password";
+
+    /**
+     * Uses a random salt value.  Every encode run will result in a different hashed value
+     */
+    @Test
+    void testLdap() {
+        PasswordEncoder ldap = new LdapShaPasswordEncoder();
+        System.out.println(ldap.encode(PASSWORD));
+        System.out.println(ldap.encode(PASSWORD));
+        System.out.println(ldap.encode(PASSWORD));
+        String encodedPassword = ldap.encode(PASSWORD);
+
+        assertTrue(ldap.matches(PASSWORD, encodedPassword));
+    }
     @Test
     void testNoOp() {
         PasswordEncoder noOp = NoOpPasswordEncoder.getInstance();
