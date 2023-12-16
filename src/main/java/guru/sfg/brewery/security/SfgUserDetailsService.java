1f1c56f18f4e5294b015b717d5f4a16e864225e5
weoamathior <weoamathior@gmail.com>
2023-12-16 08:30:48 -0800

    update user details service

--- src/main/java/guru/sfg/brewery/security/SfgUserDetailsService.java
+++ src/main/java/guru/sfg/brewery/security/SfgUserDetailsService.java
@@ -1,23 +1,43 @@
 package guru.sfg.brewery.security;
 
+import guru.sfg.brewery.domain.security.Authority;
 import guru.sfg.brewery.domain.security.User;
 import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SfgUserDetailsService implements UserDetailsService {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        Optional<User> user = userRepository.findByUsername(s);
-        if (user.isEmpty()) throw new UsernameNotFoundException("username not found");
-        return user.get();
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username + " not found"));
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                user.isEnabled(), user.isAccountNonExpired(), user.isCredentialsNonExpired(),
+                user.isAccountNonLocked(), convertSpringAuthorities(user.getAuthorities()));
+    }
+
+    private Collection<? extends GrantedAuthority> convertSpringAuthorities(Set<Authority> authorities) {
+        if (Objects.nonNull(authorities) && authorities.size() > 0) {
+            return authorities.stream()
+                    .map(Authority::getAuthority)
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toSet());
+        }
+        return Collections.emptySet();
     }
 }
