003f8e7d575844bab839ef921b084bba8bddc179
weoamathior <weoamathior@gmail.com>
2023-12-21 07:17:06 -0600

    refactor load users

--- src/main/java/guru/sfg/brewery/bootstrap/UserLoader.java
+++ src/main/java/guru/sfg/brewery/bootstrap/UserLoader.java
@@ -1,15 +1,19 @@
 package guru.sfg.brewery.bootstrap;
 
 import guru.sfg.brewery.domain.security.Authority;
+import guru.sfg.brewery.domain.security.Role;
 import guru.sfg.brewery.domain.security.User;
 import guru.sfg.brewery.repositories.security.AuthorityRepository;
+import guru.sfg.brewery.repositories.security.RoleRepository;
 import guru.sfg.brewery.repositories.security.UserRepository;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
@@ -17,37 +21,42 @@ import org.springframework.stereotype.Component;
 public class UserLoader implements CommandLineRunner {
     private final UserRepository userRepository;
     private final AuthorityRepository authorityRepository;
+    private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
     @Override
     public void run(String... args) throws Exception {
         log.info("Running userloader");
-        Authority adminAuth = Authority.builder().authority("ROLE_ADMIN").build();
-        Authority userAuth = Authority.builder().authority("ROLE_USER").build();
-        Authority customerAuth = Authority.builder().authority("ROLE_CUSTOMER").build();
-        authorityRepository.save(adminAuth);
-        authorityRepository.save(userAuth);
-        authorityRepository.save(customerAuth);
+        Authority createBeer = authorityRepository.save(Authority.builder().authority("beer.create").build());
+        Authority updateBeer = authorityRepository.save(Authority.builder().authority("beer.update").build());
+        Authority deleteBeer = authorityRepository.save(Authority.builder().authority("beer.delete").build());
+        Authority readBeer = authorityRepository.save(Authority.builder().authority("beer.read").build());
+
+        Role adminRole = roleRepository.save(Role.builder().name("ADMIN").build());
+        Role customerRole = roleRepository.save(Role.builder().name("CUSTOMER").build());
+        Role userRole = roleRepository.save(Role.builder().name("USER").build());
+
+        adminRole.setAuthorities(Set.of(createBeer,updateBeer,deleteBeer,readBeer));
+        customerRole.setAuthorities(Set.of(readBeer));
+        userRole.setAuthorities(Set.of(readBeer));
+
+        roleRepository.saveAll(Arrays.asList(adminRole,customerRole, userRole));
 
         User adminUser = User.builder()
                 .username("spring")
                 .password(passwordEncoder.encode("guru"))
-                .authority(adminAuth).build();
+                .role(adminRole).build();
         User userUser= User.builder()
                 .username("user")
                 .password(passwordEncoder.encode("password"))
-                .authority(userAuth).build();
+                .role(userRole).build();
         User scott= User.builder()
                 .username("scott")
                 .password(passwordEncoder.encode("tiger"))
-                .authority(customerAuth).build();
+                .role(customerRole).build();
         userRepository.save(adminUser);
         userRepository.save(userUser);
         userRepository.save(scott);
 
         log.info("how many? " + userRepository.count());
-//                .withUser("spring").password("{bcrypt}$2a$16$L8UgxnnceGLrMNkJpxYQLuY6UL9MYmER4N8JSuyDnNb9L7UyQGYu6").roles("ADMIN").and()
-//                .withUser("user").password("{sha256}8a212c3cde9005dd9a52044794e16d7bc879334907489c42ab30a2b1b6cd56290d817859281810dd").roles("USER")
-//                .and()
-//                .withUser("scott").password("{bcrypt10}$2a$10$hjI0kXS/ZH.zPyII9CTNZ.ZfM2wDkSz.5Cht06MNhm97TavYxwkvy").roles("CUSTOMER");
     }
 }
