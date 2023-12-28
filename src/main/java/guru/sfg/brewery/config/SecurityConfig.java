f0c40c4fa0e9958fb0d9406d7d5d3f557d109df2
weoamathior <weoamathior@gmail.com>
2023-12-28 06:50:49 -0600

    update security config to use custom login flow

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -43,8 +44,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 })
                 .authorizeRequests()
                 .anyRequest().authenticated()
-                .and().formLogin()
-                .and().httpBasic()
+                .and().formLogin(loginConfigurer -> {
+                    loginConfigurer.loginProcessingUrl("/login")
+                            .loginPage("/").permitAll()
+                            .successForwardUrl("/")
+                            .defaultSuccessUrl("/");
+                })
+                .logout(logoutConfigurer -> {
+                    logoutConfigurer.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                            .logoutSuccessUrl("/").permitAll();
+                })
+                .httpBasic()
                 .and().csrf().ignoringAntMatchers("/h2-console/**", "/api/**");
 
         // h2-console config (spring security blocks frames)
