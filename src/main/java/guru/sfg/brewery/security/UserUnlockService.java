ee5af939abccb8e48e148acdcb3cf57f3d82fd14
weoamathior <weoamathior@gmail.com>
2023-12-29 11:08:18 -0600

    using task scheduler to unlock accounts in the background

---
+++ src/main/java/guru/sfg/brewery/security/UserUnlockService.java
@@ -0,0 +1,37 @@
+package guru.sfg.brewery.security;
+
+import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserUnlockService {
+    private final UserRepository userRepository;
+
+    @Scheduled(fixedRate = 5000)
+    public void unlockAccounts() {
+//        log.debug("running unlock accounts");
+
+        List<User> users = userRepository
+                .findAllByAccountNonLockedAndLastModifiedDateBefore(false, Timestamp.valueOf(LocalDateTime.now().minusSeconds(30)));
+
+        if (users.size() > 0) {
+            log.debug("found locked users");
+            users.forEach(u -> u.setAccountNonLocked(true));
+
+            userRepository.saveAll(users);
+        }
+
+
+    }
+
+}
