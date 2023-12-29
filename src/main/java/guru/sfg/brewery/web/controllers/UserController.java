956c5c80439462d29fb57b854b186113ab6eacde
weoamathior <weoamathior@gmail.com>
2023-12-29 14:50:15 -0600

    add register 2fa logic to controller

--- src/main/java/guru/sfg/brewery/web/controllers/UserController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/UserController.java
@@ -36,14 +36,26 @@ public class UserController {
         return "user/register2fa";
     }
 
+    @PostMapping("/register2fa")
+    public String confirm2fa(@RequestParam Integer verifyCode) {
+        log.debug("verifyCode = " + verifyCode);
+        User user = getUser();
+        if (googleAuthenticator.authorizeUser(user.getUsername(), verifyCode)) {
+            User savedUser = userRepository.findById(user.getId()).orElseThrow();
+            savedUser.setUseGoogle2fa(true);
+            userRepository.save(savedUser);
+            return "index";
+
+        } else {
+            log.debug("oh no... that's a bad code");
+            // bad code
+            return "user/register2fa";
+        }
+    }
+
     private User getUser() {
         return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 
-    @PostMapping
-    public String confirm2fa(@RequestParam Integer verifyCode) {
-        log.debug("verifyCode = " + verifyCode);
-        return "index";
-    }
 
 }
