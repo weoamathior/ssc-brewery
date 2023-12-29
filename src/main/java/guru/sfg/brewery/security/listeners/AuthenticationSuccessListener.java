cb79a2da2714bbd5f4f21976b6459b850f271683
weoamathior <weoamathior@gmail.com>
2023-12-29 06:59:53 -0600

    add login success entity and repo and linking it to the auth success event

--- src/main/java/guru/sfg/brewery/security/listeners/AuthenticationSuccessListener.java
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationSuccessListener.java
@@ -1,6 +1,9 @@
 package guru.sfg.brewery.security.listeners;
 
+import guru.sfg.brewery.domain.security.LoginSuccess;
 import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.LoginSuccessRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -10,23 +13,32 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class AuthenticationSuccessListener {
+
+    private final LoginSuccessRepository loginSuccessRepository;
 
     @EventListener
     public void listen(AuthenticationSuccessEvent event) {
         log.debug("User logged in okay");
         if (event.getSource() instanceof UsernamePasswordAuthenticationToken) {
+            LoginSuccess.LoginSuccessBuilder builder = LoginSuccess.builder();
+
             UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
             if (token.getPrincipal() instanceof User) {
                 User user = (User) token.getPrincipal();
                 log.debug("user is " + user.getUsername());
+                builder.user(user);
             }
 
             if (token.getDetails() instanceof WebAuthenticationDetails) {
                 WebAuthenticationDetails details = (WebAuthenticationDetails) token.getDetails();
                 log.debug("remote addr: " + details.getRemoteAddress());
+                builder.sourceIp(details.getRemoteAddress());
 
             }
+            LoginSuccess success = loginSuccessRepository.save(builder.build());
+            log.debug("saved success " + success.getId());
         }
     }
 }
